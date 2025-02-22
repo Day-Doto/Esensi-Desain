e3d7acc53fe8ab6a3a17a08941f20dc1ef1b3ebf
Day-Doto <lerrywayangkau@gmail.com>
2025-02-22 12:06:25 +0700

    menambahkan file dan folder image

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.toggle("active");
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
