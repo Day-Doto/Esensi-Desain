04a2526870ea0a86ff71fb6cc3ccad28060ca039
Day-Doto <lerrywayangkau@gmail.com>
2025-02-22 16:05:49 +0700

    melakukan modifikasi file index file js dan file css

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.toggle("active");
   });
 });
-
-document.addEventListener("scroll", function () {
-  let img = document.querySelector("img");
-  img.style.transform = "translateY(" + window.scrollY * 0.1 + "px)";
-});
